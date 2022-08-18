3227ea9622c51d410d1a08584deaa2019619bfc6
mercuri <mercuri.abb@gmail.com>
2022-08-18 16:53:55 +0600

    Implement display behavior for buttons except =

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".button");
+const clearBtn = document.querySelector(".button.cl");
+const backBtn = document.querySelector(".button.back");
+
+let displayContent = "";
+
+let buttonsToShow = Array.from(buttons).filter(button => !(button.classList.contains("special")));
+buttonsToShow.forEach(button => button.addEventListener("click", () => populateDisplay(button.getAttribute("data-btn"))));
+clearBtn.addEventListener("click", () => clearDisplay());
+backBtn.addEventListener("click", () => popFromDisplay());
+
+
+function populateDisplay(item) {
+  displayContent += item;
+  display.textContent = displayContent;
+}
+
+
+function clearDisplay() {
+  displayContent = "";
+  display.textContent = "0";
+}
+
+
+function popFromDisplay() {
+  displayContent = displayContent.slice(0, this.length-1);
+  display.textContent = displayContent;
+}
+
+
 function add(a, b) {
   return a + b;
 }
