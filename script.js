0c550ff60dafaeef5d5c2e1b0ece136bdf8857cb
mercuri <mercuri.abb@gmail.com>
2022-08-27 18:25:16 +0600

    Add some comments to script.js

--- script.js
+++ script.js
@@ -5,30 +5,32 @@ const backBtn = document.querySelector(".button.back");
 const eqBtn = document.querySelector(".button.eq");
 
 let displayContent = "";
+
+// inputStack is used to keep track of buttons
+// that have been clicked.
 inputStack = [];
 
+// buttonsToShow is an array containing buttons that
+// must display their value when clicked, as opposed to
+// speial buttons with unique behaviors (clear, equals, back, etc.).
 let buttonsToShow = Array.from(buttons).filter(button => !(button.classList.contains("special")));
 buttonsToShow.forEach(button => button.addEventListener("click", () => populateDisplay(button)));
+
+// Implement the behavior of special buttons
 clearBtn.addEventListener("click", () => clearDisplay());
 backBtn.addEventListener("click", () => popFromDisplay());
 eqBtn.addEventListener("click", () => showResult());
 
 
-function populateDisplay(item) {
-  if ((item.textContent === "0") && inputStack.length === 0) {
+function populateDisplay(button) {
+  if ((button.textContent === "0") && inputStack.length === 0) {
     return;
   }
 
-  inputStack.push(item);
+  inputStack.push(button);
   updateDisplay();
 }
 
-
-function showResult() {
-  let result = evaluateExpression(createExpressionObject(createExpressionArray()));
-  display.textContent = result;
-  inputStack = [];
-}
 
 function clearDisplay() {
   inputStack = [];
@@ -44,6 +46,30 @@ function popFromDisplay() {
   updateDisplay();
 }
 
+
+// updateDisplay: A universal helper function that updates
+//                displayContent variable and the display
+//                depending on the input stack.
+function updateDisplay() {
+  displayContent = inputStack.map(button => button.getAttribute("data-btn")).join("");
+  if (displayContent === "") {
+    display.textContent = "0";
+  }
+  else {
+    display.textContent = displayContent;
+  }
+}
+
+
+function showResult() {
+  let result = evaluateExpression(createExpressionObject(createExpressionArray()));
+  display.textContent = result;
+  inputStack = [];
+}
+
+
+// The following are simple functions used to
+// operate on simple expressions.
 
 function add(a, b) {
   return a + b;
@@ -75,17 +101,22 @@ function operate(op, a, b) {
 }
 
 
-function updateDisplay() {
-  displayContent = inputStack.map(button => button.getAttribute("data-btn")).join("");
-  if (displayContent === "") {
-    display.textContent = "0";
-  }
-  else {
-    display.textContent = displayContent;
-  }
-}
+// The following are the main functions for evaluating the given expression.
+// The goal is to create an object representation of the main expression that
+// stores its operands and operator. It can contain other similar objects as
+// operands (meaning that the expression contains smaller expressions).
+// Operator precedence is taken into accound while creating such objects.
+// When the object is ready, we evaluate it by operating on it and operating
+// on all of its child expressions (if present).
 
-
+// createExpressionArray: Creates an array of objects, each having type and
+//                        value properties. Type can be either of two:
+//                        op (operator) or number.
+//                        It uses the input stack and its output is basically
+//                        an array representation of the expression.
+//                        It is an intermediate process for convenience and is
+//                        used while creating the main data structure of the
+//                        program - the expression object.
 function createExpressionArray() {
   expressionArray = [];
 
@@ -105,11 +136,17 @@ function createExpressionArray() {
 }
 
 
+// createExpressionObject: Creates an expression object from an
+//                         expression array.
+//                         Expression object is an object representation
+//                         of the expression, with properties opn1 (operand-1),
+//                         opn2 (operand-2), and op (operator).
+//                         Operands of the expression can be other smaller expressions.
 function createExpressionObject(expressionArray) {
   let expressionObject = {};
   let len = expressionArray.length;
 
-  // If last operator is low-precedence
+  // If last operator is low-precedence:
   if (expressionArray[len-2].value === "+" || expressionArray[len-2].value === "-") {
     expressionObject.op = expressionArray[len-2].value;
     expressionObject.opn2 = expressionArray[len-1].value;
@@ -141,11 +178,12 @@ function createExpressionObject(expressionArray) {
 
   }
 
-  console.log(expressionObject);
   return expressionObject;
 }
 
 
+// chainExpression: Used to quickly create an expression object
+//                  from a chain of high-precedence operations.
 function chainExpression(expressionArray) {
   let expressionObject = {};
   let len = expressionArray.length;
@@ -165,6 +203,10 @@ function chainExpression(expressionArray) {
 }
 
 
+// evaluateExpression: Takes an expression object as input and
+//                     recursively operates on all of its expressions
+//                     (via operate(op, opn1, opn2)).
+//                     Returns the result of evaluation as a number.
 function evaluateExpression(expressionObject) {
   if (typeof(expressionObject) === "number") {
     return expressionObject;
